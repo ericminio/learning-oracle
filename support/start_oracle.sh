30cc8b7b91cfbba0aa07e17855f874653ed45b7c
Eric Mignot <eric.mignot@gmail.com>
2019-01-31 16:25:08 -0800

    acknowledge sql and support folders are parallel

--- support/start_oracle.sh
+++ support/start_oracle.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker run --name oracle -v $DIR/sql:/tmp/sql -p 49161:1521 -p 8080:8080 wnameless/oracle-xe-11g
+docker run --name oracle -v $DIR/../sql:/tmp/sql -p 49161:1521 -p 8080:8080 wnameless/oracle-xe-11g
