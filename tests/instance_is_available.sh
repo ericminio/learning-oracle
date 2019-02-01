ef6a9c830f42325e42495b37bd205b29f9c4e7e6
Eric Mignot <eric.mignot@gmail.com>
2019-01-31 16:34:03 -0800

    fix indentation

--- tests/instance_is_available.sh
+++ tests/instance_is_available.sh
@@ -5,14 +5,14 @@ DIR=/tmp/sql
 OUTPUT=$(docker exec oracle /tmp/sql/sqlplus.sh $DIR/select.connection.info.sql)
 if [[ $OUTPUT != *"XE		 OPEN	      ACTIVE"* ]]; then
     echo "FAILURE"
-	echo "OUTPUT WAS: $OUTPUT"
+    echo "OUTPUT WAS: $OUTPUT"
     exit 1
 fi
 
 OUTPUT=$(docker exec oracle /tmp/sql/sqlplus.sh $DIR/try.some.data.stuff.sql)
 if [[ $OUTPUT != *"3 new day"* ]]; then
     echo "FAILURE"
-	echo "OUTPUT WAS: $OUTPUT"
+    echo "OUTPUT WAS: $OUTPUT"
     exit 1
 fi
 
